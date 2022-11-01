dcbe7b8e1c99f04904e4e07c5f138ee39038aed9
wjtyutzy <wang.jing16@northeastern.edu>
2022-11-01 11:34:45 -0700

    checking in initial test 2 on Nov 1

---
+++ InitialTest2/InitialTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InitialTest2
+//
+//  Created by laputer on 11/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    //try another time on Nov 1
+}
+
